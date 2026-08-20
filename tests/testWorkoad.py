de08b3528f74adcab59d20e24b561e30aa263e33
kalwardinX <akashrawat44444@gmail.com>
2026-08-21 03:17:01 +0530

    Added test_baseline

--- tests/testWorkoad.py
+++
@@ -1,6 +0,0 @@
-from auto_index_selector.workload import getWorkload
-from pyprojroot import here
-
-workloadPath = str(here() / "workload" / "queries_tpch")
-
-print(getWorkload(workloadPath=workloadPath))
