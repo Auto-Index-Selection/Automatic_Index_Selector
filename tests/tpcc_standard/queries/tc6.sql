7b4a21578c25a4a8b11a05b8d208e43a57a5e9bd
Pratik Wadekar <pratikwadekar88@gmail.com>
2026-08-28 15:09:53 +0530

    fix: update tc6 customer last name predicate to match 3-syllable standard

--- tests/tpcc_standard/queries/tc6.sql
+++ tests/tpcc_standard/queries/tc6.sql
@@ -2,6 +2,6 @@
 SELECT c.c_id, c.c_first, c.c_balance, o.o_id, o.o_entry_d 
 FROM customer c 
 JOIN orders o ON c.c_w_id = o.o_w_id AND c.c_d_id = o.o_d_id AND c.c_id = o.o_c_id 
-WHERE c.c_w_id = 1 AND c.c_d_id = 2 AND c.c_last = 'ABLEBAR' 
+WHERE c.c_w_id = 1 AND c.c_d_id = 2 AND c.c_last = 'BARBARESE' 
 ORDER BY o.o_id DESC 
 LIMIT 1;
